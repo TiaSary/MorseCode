b93ff560728247d28323ef137d28467da654e19c
TiaSary <145559326+TiaSary@users.noreply.github.com>
2023-11-20 04:48:42 +0200

    define the text_to_morse function

--- Morse.py
+++ Morse.py
@@ -1,7 +1,21 @@
+# Define a dictionary for converting from text to morse
 ttm = {'A' : '.-', 'B' : '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
     'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
     'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
     'Y': '-.--', 'Z': '--..', 'a' : '.-', 'b' : '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f': '..-.', 'g': '--.', 'h': '....',
     'i': '..', 'j': '.---', 'k': '-.-', 'l': '.-..', 'm': '--', 'n': '-.', 'o': '---', 'p': '.--.',
     'q': '--.-', 'r': '.-.', 's': '...', 't': '-', 'u': '..-', 'v': '...-', 'w': '.--', 'x': '-..-',
-    'y': '-.--', 'z': '--..'}
+    'y': '-.--', 'z': '--..', ' ' : ' '}
+
+
+# Define a function that converts text to morse
+def text_to_morse(text):
+    morse_code = ""
+
+    for letter in ttm:
+        morse_letter = ttm.get(letter)
+        morse_code = morse_code + morse_letter
+    return morse_code
+
+
+
