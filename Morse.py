111c9704d896ab405106665a7606256d1ad17e03
TiaSary <145559326+TiaSary@users.noreply.github.com>
2023-11-20 05:13:09 +0200

    update working encryption done

--- Morse.py
+++ Morse.py
@@ -5,19 +5,20 @@ ttm = {'A' : '.-', 'B' : '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.',
     'Y': '-.--', 'Z': '--..', 'a' : '.-', 'b' : '-...', 'c': '-.-.', 'd': '-..', 'e': '.', 'f': '..-.', 'g': '--.', 'h': '....',
     'i': '..', 'j': '.---', 'k': '-.-', 'l': '.-..', 'm': '--', 'n': '-.', 'o': '---', 'p': '.--.',
     'q': '--.-', 'r': '.-.', 's': '...', 't': '-', 'u': '..-', 'v': '...-', 'w': '.--', 'x': '-..-',
-    'y': '-.--', 'z': '--..', ' ' : ' '}
+    'y': '-.--', 'z': '--..', ' ' : '|'}
 
 
 # Define a function that converts text to morse
 def text_to_morse(text):
     morse_code = ""
 
-    for letter in ttm:
+    for letter in text:
         morse_letter = ttm.get(letter)
-        morse_code = morse_code + morse_letter
-    return morse_code
+        morse_code += morse_letter + "  "
+    return morse_code.strip()
 
 
+# Execution for text to morse
 text = input("Enter text to convert to Morse Code: ")
 morse_code = text_to_morse(text)
 print("Morse Code: ", morse_code)
