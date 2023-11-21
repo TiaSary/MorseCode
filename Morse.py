b75fb1c249e2da5ea7db5c47443d7cb96367cc43
TiaSary <145559326+TiaSary@users.noreply.github.com>
2023-11-21 04:44:16 +0200

    attempt at adding application, not working

--- Morse.py
+++ Morse.py
@@ -25,7 +25,7 @@ def text_to_morse(text):
     morse_code = ""
 
     for text_letter in text:
-        morse_letter = ttm.get(text_letter)
+        morse_letter = ttm.get(text_letter, '#') 
         morse_code += morse_letter + " "
     return morse_code.strip()
 
@@ -33,17 +33,17 @@ def text_to_morse(text):
 
 # Define a function that converts from morse to text
 def morse_to_text(morse_code):
-    morse_list = morse_code.split(" ")
+    morse_list = morse_code.split(' ') #splits string of morse to individual sequences
     text = ""
     
     for morse_letter in morse_list:
-        text_letter = mtt.get(morse_letter)
+        text_letter = mtt.get(morse_letter, '#') 
         text += text_letter
-    return text
-        
+    return text.strip()
 
 
-# Execution for text to morse function
+
+# Execution of text to morse function
 text = input("Enter text to encrypt to Morse Code: ")
 morse_code = text_to_morse(text)
 print("Morse Code: ", morse_code)
@@ -51,6 +51,43 @@ print("Morse Code: ", morse_code)
 
 
 # Execution for morse to text function
-morse_code = input("Enter Morse Code to decrypt to text")
+morse_code = input("Enter Morse Code to decrypt to text: ")
 text = morse_to_text(morse_code)
 print("Text = ", text)
+
+
+
+
+
+
+
+
+
+"""# Application
+print("This application has been designed to encypt your text to Morse Code, and decrypt your Morse Code to text.")
+print('Enter "E" to encrypt, "D" to decrypt, and "Q" to quit the application.')
+mode = input()
+"""
+"""if mode == "e" or "E":
+    # Execution of text to morse function
+    text = input("Enter text to encrypt to Morse Code: ")
+    morse_code = text_to_morse(text)
+    print("Morse Code: ", morse_code)
+
+elif mode == "D" or "d":
+    # Execution for morse to text function
+    morse_code = input("Enter Morse Code to decrypt to text")
+    text = morse_to_text(morse_code)
+    print("Text = ", text.strip())
+
+elif mode == "Q" or "q":
+    print("Quitting the application")
+
+else:
+    print("Please enter a valid response.")"""
+
+
+
+
+
+
