cc08cb43b647d2503bd13d5250a44b0540048917
TiaSary <145559326+TiaSary@users.noreply.github.com>
2023-11-22 16:14:22 +0200

    add unit testing

---
+++ unittesting.py
@@ -0,0 +1,25 @@
+import unittest
+import Morse
+
+class TestMorseCode(unittest.TestCase):
+
+
+    def test_text_to_morse(self):
+        self.assertEqual(Morse.text_to_morse("tia"), "- .. .-")
+
+    def test_morse_to_text(self):
+        self.assertEqual(Morse.morse_to_text("- .. .-"), "tia")
+
+    def test_invalid_text_to_morse(self):
+        self.assertEqual(Morse.text_to_morse("$$%@!"), "# # # # #")
+
+    def test_invalid_morse_to_text(self):
+        self.assertEqual(Morse.morse_to_text("---. -.-.-.--.-. -....-"), "###")
+
+
+
+
+
+
+if __name__ == '__main__': 
+    unittest.main()
